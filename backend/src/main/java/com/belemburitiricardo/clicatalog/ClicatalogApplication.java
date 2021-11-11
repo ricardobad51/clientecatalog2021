18507f175f020e30caa9261a9a720e3bf423141b
ricardobad51 <rbbelem@gmail.com>
2021-11-10 23:10:30 -0300

    Backend project created

---
+++ backend/src/main/java/com/belemburitiricardo/clicatalog/ClicatalogApplication.java
@@ -0,0 +1,13 @@
+package com.belemburitiricardo.clicatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClicatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/belemburitiricardo/clicatalog/ClicatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belemburitiricardo.clicatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
